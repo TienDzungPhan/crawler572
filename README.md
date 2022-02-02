e7740bb1caf8b90a586d30a3ccc988c94482c7af
Dzung Phan <tiendzungphan@gmail.com>
2022-02-02 02:07:54 -0800

    Initialization

---
+++ README.md
@@ -0,0 +1,1 @@
+### A Java Crawler Program with crawler4j
---
+++ src/main/java/com/td/crawler572/App.java
@@ -0,0 +1,13 @@
+package com.td.crawler572;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
